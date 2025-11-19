e54e58acb1cd6874bace9f6b4790e740dddb4c27
sidhubad143 <lakhwinder4332@gmail.com>
2025-11-19 10:02:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 # config.py
 OWNER_ID = 7616808278       # Apna Telegram ID paa de
 BOT_TOKEN = "8461515700:AAG9fNfaFOA3G6bWpsRCGMijuabIKsopmEI"
+
+API_ID = 25742938                  # ← change kar
+API_HASH = "b35b715fe8dc0a58e8048988286fc5b6"
